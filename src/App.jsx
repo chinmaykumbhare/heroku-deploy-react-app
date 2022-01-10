aa2ce03a6a6e4579456f57a6a278e344f63913d2
Chinmay Kumbhare <thechinmaykumbhare@gmail.com>
2022-01-10 11:26:33 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h3>Hello World!</h3>
+    </div>
+  )
+}
+
+export default App
